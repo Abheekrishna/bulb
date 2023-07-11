da6aaccc405f4b923d05285238a722a056e404f1
Abheekrishna <abheekrishna17@gmail.com>
2023-07-11 20:26:30 +0530

    Styled button

--- app.js
+++ app.js
@@ -2,8 +2,28 @@ const imageDiv = document.getElementById('images');
 const button = document.getElementById('button');
 const bulb = document.getElementById('bulb');
 
+
+let isOn = false;
+
 const on = () => {
     bulb.src = './images/bulb-on.png';
     bulb.classList.add('on');
+    isOn = true;
 }
+
+const off = () => {
+    bulb.src = './images/bulb-off.png';
+    bulb.classList.remove('on');
+    isOn = false;
+}
+
+const toggle = () => {
+    if(isOn) {
+        on();
+    } else {
+        off();
+    }
+}
+
+button.addEventListener('click', toggle);
 
