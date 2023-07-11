0e153c86da4d228b57fe80caf5c21828d1fa1a7d
Abheekrishna <abheekrishna17@gmail.com>
2023-07-11 20:18:29 +0530

    Added bulb on effect

--- app.js
+++ app.js
@@ -2,3 +2,8 @@ const imageDiv = document.getElementById('images');
 const button = document.getElementById('button');
 const bulb = document.getElementById('bulb');
 
+const on = () => {
+    bulb.src = './images/bulb-on.png';
+    bulb.classList.add('on');
+}
+
